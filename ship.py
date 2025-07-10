fa9132cbafef834a4bb371cbc88aab2ac8d331ff
LiZiChen <li_zi_chen@qq.com>
2025-07-10 17:58:44 +0800

    7.10下班提交

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船并获取其外接矩形
@@ -15,7 +16,26 @@ class Ship:
         # 将飞船放置在屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 移动标志（飞船一开始不移动）
+        self.moving_right = False
+        self.moving_left = False
+
+        # 在飞船的属性xy中存储一个浮点数
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
+    
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        # 更新飞船的x值，而不是rect对象
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        # 更新rect对象的位置
+        self.rect.x = self.x
