6c4a45b4baaf54bf25e0a709f6f48498b6362043
LiZiChen <li_zi_chen@qq.com>
2025-07-12 11:53:02 +0800

    添加了外星人移动功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,10 +8,10 @@ from alien import Alien
 
 
 class AlienInvasion:
-    """Overall class to manage game assets and behavior."""
+    """管理游戏资源和行为的类"""
 
     def __init__(self):
-        """Initialize the game, and create game resources."""
+        """初始化游戏并创建游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
@@ -26,16 +26,25 @@ class AlienInvasion:
 
         
     def run_game(self):
-        """Start the main loop for the game."""
+        """开始游戏的主循环"""
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
-    
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+
     def _check_keydown_events(self, event):
         """处理按键按下事件"""
         if event.key == pygame.K_RIGHT:
@@ -46,6 +55,14 @@ class AlienInvasion:
             self._fire_bullet()
         elif event.key == pygame.K_ESCAPE:
             sys.exit()
+
+
+    def _check_keyup_events(self, event):
+        """处理按键松开事件"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     
     
     def _update_bullets(self):
@@ -58,44 +75,10 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
     
 
-    def _check_keyup_events(self, event):
-        """处理按键松开事件"""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-
-
-    def _check_events(self):
-        """响应按键和鼠标事件"""
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
-
-
-    def _create_fleet(self):
-        """创建外星人舰队"""
-        # 创建一个外星人
-        alien = Alien(self)
-        alien_width = alien.rect.width
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
-        self.aliens.add(alien)
-
-    
-    def _create_alien(self,x_position):
-        """创建一个新的外星人并添加到编组中"""
-        new_alien = Alien(self)
-        new_alien.x = x_position
-        new_alien.rect.x = x_position
-        self.aliens.add(new_alien)
+    def _update_aliens(self):
+        """检查是否有外星人位于屏幕边缘，并更新外星舰队中所有外星人的位置"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
 
     def _update_screen(self):
@@ -107,6 +90,47 @@ class AlienInvasion:
         self.aliens.draw(self.screen)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
+
+    def _create_fleet(self):
+        """创建外星人舰队"""
+        # 创建一个外星人，再不断添加，直到没有足够空间放置更多外星人
+        # 外星人的间距为外星人的宽度和外星人的高度
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        current_x ,current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            # 添加一行外星人后，重置current_x并增加current_y
+            current_x = alien_width
+            current_y += 2 * alien_height
+        self.aliens.add(alien)
+
+    def _create_alien(self,x_position,y_position):
+        """创建一个新的外星人并添加到编组中"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.y = y_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+    
+
+    def _check_fleet_edges(self):
+        """响应外星人到达屏幕边缘的情况"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+
+    def _change_fleet_direction(self):
+        """将外星人舰队向下移动，并改变方向"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
     
 
     def _fire_bullet(self):
